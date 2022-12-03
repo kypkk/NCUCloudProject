df86a9df4596581c239ae476e7551dc929cee1e3
kypkk <kypss50102@gmail.com>
2022-12-03 23:24:44 +0800

    ADD: Initial commit for Home page

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col max-w-screen-2xl py-2 px-4">
+      <section className=" mx-14 my-4">
+        <h1 className="text-5xl">Home</h1>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
