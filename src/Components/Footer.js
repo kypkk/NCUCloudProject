2001a19deba0eddf6517be32aabcb2dd1737b91c
kypkk <kypss50102@gmail.com>
2022-12-03 23:27:25 +0800

    ADD: Initial commit for Footer component

---
+++ src/Components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer class="text-center lg:text-left bg-gray-100 text-gray-600 mt-auto">
+      <div class="text-center p-6 bg-gray-200">
+        <span>© 2022 NCU Cloud </span>
+        <a
+          class="text-gray-800 font-semibold"
+          href="https://tailwind-elements.com/"
+        >
+          Group2
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
