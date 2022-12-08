78f6aca8891f92c8545c3dbd4e07d9e9025a0b1f
kypkk <kypss50102@gmail.com>
2022-12-09 01:18:10 +0800

    Feat: Redirect web page

--- client/src/Pages/Bekaidei.js
+++ client/src/Pages/Bekaidei.js
@@ -1,12 +1,21 @@
-import React from "react";
-import RedirComponent from "../Components/RedirComponent";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 const Bekaidei = () => {
-  console.log(window.location.pathname);
-  return (
-    <div>
-      <RedirComponent />
-    </div>
-  );
+  const [url, seturl] = useState();
+  let str = window.location.pathname;
+  str = str.replace("/Bekaidei", "");
+  str = "http://localhost:8080" + str;
+  useEffect(() => {
+    axios
+      .post(str)
+      .then((response) => {
+        seturl(response.data.URL);
+      })
+      .then(window.location.replace(url));
+  });
+
+  return <div></div>;
 };
 
 export default Bekaidei;
