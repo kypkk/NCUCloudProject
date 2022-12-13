b0706f547d262fc53b919121434e7cc3059a900d
kypkk <kypss50102@gmail.com>
2022-12-13 23:14:33 +0800

    ADD: BottomLine SERVER_PORT

--- server/app.js
+++ server/app.js
@@ -96,5 +96,5 @@ app.post("/shorten", async (req, res) => {
 });
 
 app.listen(process.env.SERVER_PORT, () => {
-  console.log("Server listening on port 8080");
+  console.log("Server listening on port " + process.env.SERVER_PORT);
 });
