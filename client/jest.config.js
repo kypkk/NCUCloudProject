a97f2decbfe48ad63d46d5aab44fae55e8d45c00
kypkk <kypss50102@gmail.com>
2023-10-17 15:38:12 +0800

    feat: test environment

--- client/jest.config.js
+++ client/jest.config.js
@@ -3,4 +3,17 @@ module.exports = {
     "^.+\\.jsx?$": "babel-jest",
   },
   testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testPathIgnorePatterns: ["(.*)index.js$", "/node_modules/", "/mocks/"],
+  coveragePathIgnorePatterns: [
+    "src/Images/",
+    "(.*)index.js$",
+    "/node_modules/",
+    "/mocks/",
+  ],
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/mocks/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/mocks/fileMock.js",
+  },
 };
